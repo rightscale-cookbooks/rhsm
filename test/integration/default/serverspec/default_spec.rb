c42c9d691ca0a6f8ba6015271d0ecfea9ad744fd
Lopaka Delp <lopaka@rightscale.com>
2014-09-25 10:06:48 -0700

    Add serverspec tests.

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -1,1 +1,9 @@
 require 'spec_helper'
+
+describe service('rhsm') do
+  it { should be_enabled }
+end
+
+describe command('subscription-manager identity') do
+  it { should return_exit_status 0 }
+end
---
+++ test/integration/unregister/serverspec/spec_helper.rb
@@ -0,0 +1,6 @@
+require 'serverspec'
+require 'pathname'
+require 'rubygems/dependency_installer'
+
+include Serverspec::Helper::Exec
+include Serverspec::Helper::DetectOS
